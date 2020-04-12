f3c7ccd897108328c5b9b8c6fb7f6d9abe1b76f5
unknown <petw213@naver.com>
2020-04-13 01:27:47 +0900

    herokuedited

--- classparser.py
+++ classparser.py
@@ -82,9 +82,9 @@ def get_class_data():
     try:
         options = webdriver.ChromeOptions()
         options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        options.add_argument("disable-gpu")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
         
 
         driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
--- html_parser.py
+++ html_parser.py
@@ -30,9 +30,9 @@ def parse_site(id, password):
     try: # html 긁어오기
         options = webdriver.ChromeOptions()
         options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        options.add_argument("disable-gpu")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
 
         driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
         driver.implicitly_wait(3)
