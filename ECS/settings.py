c23bda6601317c41f93238909e3bdb1b36e9ca74
unknown <petw213@naver.com>
2020-04-13 00:58:17 +0900

    heroku edited

--- ECS/settings.py
+++ ECS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','8a92_(wr=0o^n072z8ojn3q5re5zo-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['ecsproject2.herokuapp.com',]
 
 
 # Application definition
