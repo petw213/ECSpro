c366e88d9739e8548530910445c0f04303d86c65
unknown <petw213@naver.com>
2020-04-13 23:22:41 +0900

    'd'

--- post/models.py
+++ post/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     postTitle = models.TextField(null=True)
-    postWriter = models.CharField(null=True, max_length=20)
-    postDate = models.CharField(null=True, max_length=200)
+    postWriter = models.TextField(null=True)
+    postDate = models.TextField(null=True)
     postContent = models.TextField(null=True)
     postUrl = models.TextField(null=True)
 
